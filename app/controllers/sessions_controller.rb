ba8d7d7ce1543bac1a9a75eefdc95f058441fa09
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-25 11:06:15 +0900

    Twitter認証後の処理を行うコントローラを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,8 @@
 class SessionsController < ApplicationController
+  def create
+    p request.env['omniauth.auth']
+    # user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    # session[:user_id] = user.id
+    # redirect_to root_path, notice: 'ログインしました'
+  end
 end

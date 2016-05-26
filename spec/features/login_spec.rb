fc8cf43f01312a4b1c4905f631ac2d400d3411ab
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-26 12:31:21 +0900

    ログイン成功時のテストを追記

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 describe 'ユーザがシステムにログインをする' do
   context 'トップページの"Twiterでログイン"ボタンを押下したとき' do
+    context 'Twitterログインに成功したとき' do
+      before do
+        visit root_path
+        click_link 'Twitterでログイン'
+      end
 
+      it 'トップページに遷移すること' do
+        expect(page.current_path).to eq root_path
+      end
+
+      it '"ログインしました"と表示されること' do
+        expect(page).to have_content 'ログインしました'
+      end
+    end
   end
 end

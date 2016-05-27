25b4b4172268f1a5be9631d641feb406c0e5682f
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-27 12:12:59 +0900

    Eventモデルのスペックでテストデータの作成にFactoryGirlを使用するよう変更

---
+++ spec/factories/events.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :event do
+    name       "otofu"
+    place      "tokyo"
+    content    "content_test"
+    start_time Time.now
+    end_time   Time.now
+  end
+end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,16 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe 'validation' do
-    it "valid" do
-      event = Event.new(
-        name:       "otofsagdasdgasdgdsagadsgadsgsadllnasdjhlnsadjklfhnklasdjcfnlaksdhflcnkajsdhfnlkadsjhfnlckadshlfkasjhdlnfjaldsfhlsdkjcfladskcfnlakdsjhnfclkasdjhfnlkasdnlkfasdkljhfcnlasdkjhfu",
-        place:      "tokyo",
-        content:    "content_test",
-        start_time: Time.now,
-        end_time:   Time.now
-      )
-
-      expect(event).to be_valid
+    context 'valid' do
+      it "valid" do
+        event = FactoryGirl.create(:event)
+        expect(event).to be_valid
+      end
     end
   end
 end

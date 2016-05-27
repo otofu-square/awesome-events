03f4ad6e1256e1484c72c60cd2b1cfe19dfde448
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-27 13:32:12 +0900

    start_timeにend_timeより後の時刻が設定されていた場合のバリデーションのテストを記述

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -51,6 +51,12 @@ RSpec.describe Event, type: :model do
           expect(FactoryGirl.build(:event, end_time: '').invalid?).to be_truthy
         end
       end
+
+      describe 'start_time_should_be_before_end_time' do
+        it 'start_time >= end_time' do
+          expect(FactoryGirl.build(:event, start_time: Time.local(2016, 5), end_time: Time.local(2016, 4)).invalid?).to be_truthy    
+        end
+      end
     end
   end
 end

6eb98bfeec3373922176256ae6324cbebe450379
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-27 12:14:28 +0900

    fix double quotation to single quotation

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :event do
-    name       "otofu"
-    place      "tokyo"
-    content    "content_test"
+    name       'otofu'
+    place      'tokyo'
+    content    'content_test'
     start_time Time.now
     end_time   Time.now
   end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe 'validation' do
     context 'valid' do
-      it "valid" do
+      it 'valid' do
         event = FactoryGirl.create(:event)
         expect(event).to be_valid
       end

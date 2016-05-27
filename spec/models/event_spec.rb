f35a05bf8cd108a58d5a4daf87f5070ae0f58359
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-27 12:01:55 +0900

    Eventモデルのバリデーションに関するテストを記述

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it "valid" do
+      event = Event.new(
+        name:       "otofsagdasdgasdgdsagadsgadsgsadllnasdjhlnsadjklfhnklasdjcfnlaksdhflcnkajsdhfnlkadsjhfnlckadshlfkasjhdlnfjaldsfhlsdkjcfladskcfnlakdsjhnfclkasdjhfnlkasdnlkfasdkljhfcnlasdkjhfu",
+        place:      "tokyo",
+        content:    "content_test",
+        start_time: Time.now,
+        end_time:   Time.now
+      )
+
+      expect(event).to be_valid
+    end
+  end
 end

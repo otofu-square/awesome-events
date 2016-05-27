e2592aa2cf271d74dcb2e1a35ba1baf201ea66f7
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-27 13:15:58 +0900

    EventモデルのName以外のカラムに関するバリデーションエラーのテストを記述

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,11 +12,43 @@ RSpec.describe Event, type: :model do
     context 'invalid' do
       describe 'name' do
         it 'is too long' do
-          expect(FactoryGirl.build(:event, name: 'a'*100).invalid?).to be_truthy
+          expect(FactoryGirl.build(:event, name: 'a'*51).invalid?).to be_truthy
         end
 
         it 'is blank' do
           expect(FactoryGirl.build(:event, name: '').invalid?).to be_truthy
+        end
+      end
+
+      describe 'place' do
+        it 'is too long' do
+          expect(FactoryGirl.build(:event, place: 'a'*101).invalid?).to be_truthy
+        end
+
+        it 'is blank' do
+          expect(FactoryGirl.build(:event, place: '').invalid?).to be_truthy
+        end
+      end
+
+      describe 'content' do
+        it 'is too long' do
+          expect(FactoryGirl.build(:event, content: 'a'*201).invalid?).to be_truthy
+        end
+
+        it 'is blank' do
+          expect(FactoryGirl.build(:event, content: '').invalid?).to be_truthy
+        end
+      end
+
+      describe 'start_time' do
+        it 'is blank' do
+          expect(FactoryGirl.build(:event, start_time: '').invalid?).to be_truthy
+        end
+      end
+
+      describe 'end_time' do
+        it 'is blank' do
+          expect(FactoryGirl.build(:event, end_time: '').invalid?).to be_truthy
         end
       end
     end

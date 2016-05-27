8ac2e42456671f190f2d90e9b3b400ee8f94db27
otofu-square <yukiyakonkon1024@gmail.com>
2016-05-27 14:14:42 +0900

    Eventモデルのバリデーションのスペックをshoulda-matchersを使った書き方でリファクタリング

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,59 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe 'validation' do
+    subject { FactoryGirl.create(:event) }
+
     context 'valid' do
-      it 'valid' do
-        event = FactoryGirl.create(:event)
-        expect(event).to be_valid
-      end
+      it { is_expected.to be_valid }
     end
 
     context 'invalid' do
       describe 'name' do
-        it 'is too long' do
-          expect(FactoryGirl.build(:event, name: 'a'*51).invalid?).to be_truthy
-        end
-
-        it 'is blank' do
-          expect(FactoryGirl.build(:event, name: '').invalid?).to be_truthy
-        end
+        it { is_expected.not_to allow_value('a'*51).for(:name) }
+        it { is_expected.not_to allow_value('').for(:name) }
       end
 
       describe 'place' do
-        it 'is too long' do
-          expect(FactoryGirl.build(:event, place: 'a'*101).invalid?).to be_truthy
-        end
-
-        it 'is blank' do
-          expect(FactoryGirl.build(:event, place: '').invalid?).to be_truthy
-        end
+        it { is_expected.not_to allow_value('a'*101).for(:place) }
+        it { is_expected.not_to allow_value('').for(:place) }
       end
 
       describe 'content' do
-        it 'is too long' do
-          expect(FactoryGirl.build(:event, content: 'a'*201).invalid?).to be_truthy
-        end
-
-        it 'is blank' do
-          expect(FactoryGirl.build(:event, content: '').invalid?).to be_truthy
-        end
+        it { is_expected.not_to allow_value('a'*201).for(:content) }
+        it { is_expected.not_to allow_value('').for(:content) }
       end
 
       describe 'start_time' do
-        it 'is blank' do
-          expect(FactoryGirl.build(:event, start_time: '').invalid?).to be_truthy
-        end
+        it { is_expected.not_to allow_value('').for(:start_time) }
       end
 
       describe 'end_time' do
-        it 'is blank' do
-          expect(FactoryGirl.build(:event, end_time: '').invalid?).to be_truthy
-        end
+        it { is_expected.not_to allow_value('').for(:end_time) }
       end
 
       describe 'start_time_should_be_before_end_time' do
         it 'start_time >= end_time' do
-          expect(FactoryGirl.build(:event, start_time: Time.local(2016, 5), end_time: Time.local(2016, 4)).invalid?).to be_truthy    
+          expect(FactoryGirl.build(:event, start_time: Time.local(2016, 5), end_time: Time.local(2016, 4)).invalid?).to be_truthy
         end
       end
     end

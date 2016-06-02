8355d59f58a431fd0d1b4c74605f2869a5998023
otofu-square <yukiyakonkon1024@gmail.com>
2016-06-02 12:47:04 +0900

    ビューとしてnew.html.erbが呼ばれるようにテストと実装を書いた

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
   describe 'GET /events/new' do
-    before { get :new }
+    before do
+      get :new
+    end
 
-    it 'ステータスコードが200であること'
+    it 'ステータスコードが200であること' do
+      expect(response.status).to eq 200
+    end
 
-    it 'ビューとして new.html.erb が呼ばれること'
+    it 'ビューとして new.html.erb が呼ばれること' do
+      expect(response).to render_template 'new'
+    end
 
     it '@event に空イベントが格納されていること'
   end

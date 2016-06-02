d7797e46392bd1f5a1d06fa3286bf83e84fd03dd
otofu-square <yukiyakonkon1024@gmail.com>
2016-06-02 12:07:14 +0900

    Eventコントローラに実装する機能のテストの確認事項を記述

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  describe 'GET /events/new' do
+    before { get :new }
 
+    it 'ステータスコードが200であること'
+
+    it 'ビューとして new.html.erb が呼ばれること'
+
+    it '@event に空イベントが格納されていること'
+  end
 end

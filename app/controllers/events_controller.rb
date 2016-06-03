f34f1fce5f45c9199fa072d15ff09eba320ba6c1
otofu-square <yukiyakonkon1024@gmail.com>
2016-06-03 11:42:19 +0900

    createアクションが呼ばれた際に@eventの詳細ページにリダイレクトするように実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,13 @@
 class EventsController < ApplicationController
+  def show
+  end
+
   def new
     @event = User.find(session[:user_id]).created_events.build
+  end
+
+  def create
+    @event = User.find(session[:user_id]).created_events.build
+    redirect_to @event
   end
 end

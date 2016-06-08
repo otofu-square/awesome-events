82556ae8d2d1550691c9ad54d9a3bdcadc3a581d
otofu-square <yukiyakonkon1024@gmail.com>
2016-06-09 01:03:13 +0900

    Eventモデルのテストデータを作成

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do
   factory :event do
-    
+    sequence(:name)    {|i| "#{i}" }
+    sequence(:place)   {|i| "nickname#{i}" }
+    start_time         Time.local(2015, 1)
+    end_time           Time.local(2016, 1)
+    sequence(:content) {|i| "nickname#{i}" }
+
+    trait :with_blank do
+      name       nil
+      place      nil
+      start_time nil
+      end_time   nil
+      content    nil
+    end
   end
 end

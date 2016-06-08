29c038958bbb003b2d2bfaf0739fe0bae6640d56
otofu-square <yukiyakonkon1024@gmail.com>
2016-06-08 09:35:39 +0900

    フォーム送信されたパラメータをStrongParametersで検証する

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,13 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = User.find(session[:user_id]).created_events.build
+    @event = User.find(session[:user_id]).created_events.build(event_params)
     redirect_to @event
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :place, :start_time, :end_time, :content)
   end
 end

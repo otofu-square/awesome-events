b5951ce0155b5779f1be8cac616ec44348037323
otofu-square <yukiyakonkon1024@gmail.com>
2016-06-08 11:02:46 +0900

    デプロイ時の共有ファイル、ディレクトリの設定を記述

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,11 @@ set :scm, :git
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
